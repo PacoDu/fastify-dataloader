a7122e6b9f0cccc481def20aa153cf5fb59ec18e
Paco Dupont <paco.dupont@gmail.com>
2018-12-21 02:39:31 +0100

    Skeleton

---
+++ index.js
@@ -0,0 +1,8 @@
+'use strict'
+
+const fp = require('fastify-plugin')
+const Dataloader = require('dataloader')
+
+module.exports = fp(async function (app, opts) {
+  //
+})
---
+++ test/decorator.js
@@ -0,0 +1,29 @@
+'use strict'
+
+const { test } = require('tap')
+const Fastify = require('fastify')
+const dataloader = require('..')
+
+test('basic dataloader instance', async (t) => {
+  const app = Fastify()
+
+  app.register(dataloader, {
+    user: keys => keys.map(key => {
+      return { id: key, test: 'hello' }
+    })
+  })
+
+  app.get('/test?id=1', (request, reply) => {
+    return reply.dataloader('user').load(1)
+  })
+
+  await app.inject({
+    method: 'GET',
+    url: '/?id=1'
+  }, (err, response) => {
+    t.error(err)
+    t.strictEqual(response.statusCode, 200)
+    t.strictEqual(response.headers['content-type'], 'application/json; charset=utf-8')
+    t.deepEqual(JSON.parse(response.payload), { id: 1, hello: 'world' })
+  })
+})
